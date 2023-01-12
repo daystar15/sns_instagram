803c90305313cb95443786b93d6110783e77836f
test <test@test.com>
2023-01-12 18:11:42 +0900

    초기 환경 세팅

---
+++ src/main/java/com/sns/config/DatabaseConfig.java
@@ -0,0 +1,27 @@
+package com.sns.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration // 스프링빈이 된다
+@MapperScan(basePackages="com.sns.*")  // interface,DAO(mapper)가 있는 패키지 경로
+public class DatabaseConfig {
+
+	@Bean // springboot core선택, 리턴되는 객체를 스프링에서 자유롭게 사용할 수 있음
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+        sessionFactory.setMapperLocations(res);
+
+        return sessionFactory.getObject();
+    }
+}
